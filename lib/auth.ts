2a75c63b462656c91181886abb15168e9cfefa4f
psyger100 <rvsng028@gmail.com>
2024-09-07 03:06:43 +0530

    cookieSSR fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,53 +1,49 @@
-import {
-  CookieMethods,
-  createServerClient,
-  type CookieOptions,
-} from "@supabase/ssr";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies as nextCookies } from "next/headers";
 
 export const getUser = async () => {
-  const auth = getSupabaseAuth({ isComponent: true });
-  const user = (await auth.getUser()).data.user;
+    const auth = getSupabaseAuth({ isComponent: true });
+    const user = (await auth.getUser()).data.user;
 
-  return user;
+    return user;
 };
 
 export const protectAction = async () => {
-  const auth = getSupabaseAuth({ isComponent: true });
-  const user = (await auth.getUser()).data.user;
-  if (!user) throw new Error("Not authorized");
+    const auth = getSupabaseAuth({ isComponent: true });
+    const user = (await auth.getUser()).data.user;
+    if (!user) throw new Error("Not authorized");
 };
 
 export const getServerActionAuth = () => {
-  return getSupabaseAuth({ isComponent: false });
+    return getSupabaseAuth({ isComponent: false });
 };
 
 const getSupabaseAuth = ({ isComponent = true }: { isComponent: boolean }) => {
-  const cookieStore = nextCookies();
+    const cookieStore = nextCookies();
 
-  let cookies: CookieMethods = {
-    get(name: string) {
-      return cookieStore.get(name)?.value;
-    },
-  };
-
-  if (!isComponent) {
-    cookies = {
-      ...cookies,
-      set(name: string, value: string, options: CookieOptions) {
-        cookieStore.set({ name, value, ...options });
-      },
-      remove(name: string, options: CookieOptions) {
-        cookieStore.set({ name, value: "", ...options });
-      },
+    let cookies: any = {
+        get(name: string) {
+            return cookieStore.get(name)?.value;
+        },
     };
-  }
 
-  const client = createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
-    { cookies }
-  );
+    if (!isComponent) {
+        cookies = {
+            ...cookies,
+            set(name: string, value: string, options: CookieOptions) {
+                cookieStore.set({ name, value, ...options });
+            },
+            remove(name: string, options: CookieOptions) {
+                cookieStore.set({ name, value: "", ...options });
+            },
+        };
+    }
 
-  return client.auth;
+    const client = createServerClient(
+        process.env.SUPABASE_URL!,
+        process.env.SUPABASE_ANON_KEY!,
+        { cookies },
+    );
+
+    return client.auth;
 };
